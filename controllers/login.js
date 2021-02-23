41caed26f358be8223132e0c40b32f6823af1bfa
Robert Alvarado <treborlp@gmail.com>
2021-02-22 22:11:33 -0500

    Login de usuario

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,44 @@
-const login = async(req, res) => {
-    const post = req.body
+const bcryptjs = require("bcryptjs");
+const UsuarioSchema = require('../models/usuario');
 
-    res.json({
-        msj: post
-    })
+const login = async(req, res) => {
+
+    const { correo, password } = req.body;
+
+    try {
+        //varificar si el correo existe
+        const usuario = await UsuarioSchema.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msj: "Error en el correo o password: correo"
+            })
+        }
+        //verificar si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msj: "Error en el correo o password: status"
+            })
+        }
+        //comparar password
+        const verificarPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!verificarPassword) {
+            return res.status(400).json({
+                msj: "Error en el correo o password: password"
+            })
+        }
+
+        res.json({
+            msj: "Exito",
+            correo,
+            password
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            msj: "Error durante el proceso de login"
+        })
+    }
 
 }
 
