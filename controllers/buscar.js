9dd9926b0af24078cc8de7fb7d2e17402dcdafce
Robert Alvarado <treborlp@gmail.com>
2021-03-02 22:44:21 -0500

    Busquedas en base de daros

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,11 +1,59 @@
-const buscarController = (req, res) => {
+const { response } = require("express")
+const { ObjectId } = require("mongoose").Types;
+const UsuarioSchema = require('../models/usuario');
+
+const colecionesPermitidas = [
+    'categorias',
+    'productos',
+    'usuarios',
+    'roles'
+];
+
+const buscarUsuarios = async(termino, res = response) => {
+    const esMongoID = ObjectId.isValid(termino); //Identifica si es un id de mongo
+
+    if (esMongoID) {
+        const usuario = await UsuarioSchema.findById(termino);
+        res.json({
+            respuesta: (usuario) ? usuario : []
+        })
+    }
+}
+
+const buscarController = async(req, res) => {
 
     const { coleccion, termino } = req.params;
 
-    res.status(200).json({
-        coleccion,
-        termino
-    })
+    if (!colecionesPermitidas.includes(coleccion)) {
+        return res.status(401).json({
+            msj: "La categoria no existe"
+        })
+    }
+
+    switch (coleccion) {
+        case 'categorias':
+            res.status(200).json({
+                msj: "listamos categorias"
+            })
+            break;
+        case 'productos':
+            break;
+        case 'usuarios':
+            buscarUsuarios(termino, res)
+            break;
+        case 'roles':
+            res.status(200).json({
+                msj: "listamos roles"
+            })
+            break;
+
+        default:
+            res.status(500).json({
+                msj: "Error en el servidor"
+            })
+            break;
+    }
+
 }
 
 module.exports = buscarController
