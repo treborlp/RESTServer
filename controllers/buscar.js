49c25ce1954515d1b50314097f862057f8e7459a
Robert Alvarado <treborlp@gmail.com>
2021-03-02 23:38:54 -0500

    Busquedas completas

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,6 +1,8 @@
 const { response } = require("express")
 const { ObjectId } = require("mongoose").Types;
 const UsuarioSchema = require('../models/usuario');
+const CategoriasSchema = require('../models/categoria');
+const ProductosSchema = require('../models/producto');
 
 const colecionesPermitidas = [
     'categorias',
@@ -12,7 +14,6 @@ const colecionesPermitidas = [
 const buscarUsuarios = async(termino, res = response) => {
     const esMongoID = ObjectId.isValid(termino); //Identifica si es un id de mongo
 
-    const regex = new RegExp(termino, "i");
 
     if (esMongoID) {
         const usuario = await UsuarioSchema.findById(termino);
@@ -21,12 +22,54 @@ const buscarUsuarios = async(termino, res = response) => {
         })
     }
 
+    const regex = new RegExp(termino, "i");
+
     const usuario = await UsuarioSchema.find({
         $or: [{ nombre: regex }, { correo: regex }],
         $and: [{ estado: true }]
     });
     res.json({
         respuesta: usuario
+    })
+
+
+}
+
+const buscarCategorias = async(termino, res = response) => {
+    const esMongoID = ObjectId.isValid(termino); //Identifica si es un id de mongo
+
+
+    if (esMongoID) {
+        const categorias = await CategoriasSchema.findById(termino);
+        res.json({
+            respuesta: (categorias) ? [categorias] : []
+        })
+    }
+
+    const regex = new RegExp(termino, "i");
+
+    const categoria = await CategoriasSchema.find({ nombre: regex, estado: true });
+    res.json({
+        respuesta: categoria
+    })
+}
+
+const buscarProductos = async(termino, res = response) => {
+    const esMongoID = ObjectId.isValid(termino); //Identifica si es un id de mongo
+
+
+    if (esMongoID) {
+        const productos = await ProductosSchema.findById(termino);
+        res.json({
+            respuesta: (productos) ? [productos] : []
+        })
+    }
+
+    const regex = new RegExp(termino, "i");
+
+    const productos = await ProductosSchema.find({ nombre: regex, estado: true });
+    res.json({
+        respuesta: productos
     })
 
 
@@ -44,19 +87,13 @@ const buscarController = async(req, res) => {
 
     switch (coleccion) {
         case 'categorias':
-            res.status(200).json({
-                msj: "listamos categorias"
-            })
+            buscarCategorias(termino, res)
             break;
         case 'productos':
+            buscarProductos(termino, res)
             break;
         case 'usuarios':
             buscarUsuarios(termino, res)
-            break;
-        case 'roles':
-            res.status(200).json({
-                msj: "listamos roles"
-            })
             break;
 
         default:
