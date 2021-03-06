7ea9b55bfe222b96072f712c6d39323a8f59ce86
Robert Alvarado <treborlp@gmail.com>
2021-03-05 21:32:36 -0500

    Subir archivo controlador

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,10 +1,27 @@
 const { response } = require("express");
+const path = require('path');
 
 
 const uploads = (req, res = response) => {
-    res.status(200).json({
-        msj: "Hola todo bien"
-    })
+
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json({ msj: 'No existe ningun archivo' });
+        return;
+    }
+
+    const { archivo } = req.files;
+
+    uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+    archivo.mv(uploadPath, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ err });
+        }
+
+        res.json('File uploaded to ' + uploadPath);
+    });
 }
 
 module.exports = uploads
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
 
 class Server {
@@ -37,6 +38,11 @@ class Server {
         this.app.use(express.json());
         //Middlewar carpeta public
         this.app.use(express.static('public'));
+        //File uploads
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/'
+        }));
     }
 
 
