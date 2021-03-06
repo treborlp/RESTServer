06039750cd0758b6a6c22f73e738ac3b9f294304
Robert Alvarado <treborlp@gmail.com>
2021-03-05 21:45:36 -0500

    Validacion de formato de archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -11,6 +11,14 @@ const uploads = (req, res = response) => {
     }
 
     const { archivo } = req.files;
+    const archivoSeparado = archivo.name.split('.');
+    const extensionArchivo = archivoSeparado[archivoSeparado.length - 1]; //La extencion sel archivo se optiene mediante la ultima ubicacion del arreglo generado por split
+    const extensionesPermitidas = ['jpg', 'png', 'jpeg', 'gif']
+
+    if (!extensionesPermitidas.includes(extensionArchivo)) {
+        res.status(400).json({ msj: 'No es un archivo valido' });
+        return;
+    }
 
     uploadPath = path.join(__dirname, '../uploads/', archivo.name);
 
